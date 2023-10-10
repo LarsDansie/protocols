ae9accc09046bcc91f4d7be1cd9dbe29a244555a
Lars Dansie <larswdansie@gmail.com>
2023-10-10 09:52:52 -0600

    final

---
+++ Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+/*:
+## Exercise - Adopt Protocols: CustomStringConvertible, Equatable, and Comparable
+ 
+ Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
+ */
+import Foundation
+
+class Human: CustomStringConvertible, Equatable, Comparable, Codable {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    var description: String {
+        return "\(name)"
+    }
+    
+    static func ==(lhs: Human, rhs: Human) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+    
+    static func < (lhs: Human, rhs: Human) -> Bool {
+        return lhs.age < rhs.age
+    }
+}
+
+let myself = Human(name: "Lars", age: 25)
+let student1 = Human(name: "Austin", age: 25)
+
+//:  Make the `Human` class adopt the `CustomStringConvertible` protocol. Print both of your previously initialized `Human` objects.
+print(myself)
+print(student1)
+
+//:  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
+print(myself == student1)
+print(myself != student1)
+
+//:  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
+let ranOutOfIdeas = Human(name: "Obama", age: 62)
+let thisDude = Human(name: "Mark Ruffalo", age: 55)
+let theCreatorOfHotCheetos = Human(name: "Richard Montañez", age: 65)
+
+let people = [myself, student1, ranOutOfIdeas, thisDude, theCreatorOfHotCheetos]
+
+let sortedPeople: [Human] = people.sorted(by: <)
+print(sortedPeople)
+
+//:  Make the `Human` class adopt the `Codable` protocol. Create a `JSONEncoder` and use it to encode as data one of the `Human` objects you have initialized. Then use that `Data` object to initialize a `String` representing the data that is stored, and print it to the console.
+let jsonEncoder = JSONEncoder()
+if let jsonData = try? jsonEncoder.encode(thisDude),
+let jsonString = String(data: jsonData, encoding: .utf8) {
+    print(jsonString)
+}
+
+/*:
+page 1 of 5  |  [Next: App Exercise - Printable Workouts](@next)
+ */
